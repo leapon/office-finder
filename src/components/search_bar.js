1467278eb3762f91463ee09855e24df9c1071eb5
leapon <leapon@gmail.com>
2017-07-31 20:40:17 -0400

    add input change handler in search_bar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class SearchBar extends Component {
+  onInputChange(event) {
+    console.log('input change:', event.target.value);
+  }
   render () {
     return (
       <div>
-        <input />
+        <input onChange={ this.onInputChange }/>
       </div>
     )
   }
