58779547d2af4f07c8f20f7861990a3fe2d1770a
leapon <leapon@gmail.com>
2017-07-31 23:29:12 -0400

    add state to search_bar component; change to controlled component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class SearchBar extends Component {
-  onInputChange(event) {
-    console.log('input change:', event.target.value);
+  constructor(props) {
+    super(props);
+    this.state = { term:'' };
   }
   render () {
     return (
       <div>
-        <input onChange={ this.onInputChange }/>
+        <input
+          value= { this.state.term }
+          onChange={ event => this.setState({ term:event.target.value }) }/>
+        <div>
+        input: { this.state.term }
+        </div>
       </div>
     )
   }
