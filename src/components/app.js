8501aff1c485c44647adbb4f0b99e048e3f445b3
leapon <leapon@gmail.com>
2017-08-01 23:45:32 -0400

    set offices using search term

--- src/components/app.js
+++ src/components/app.js
@@ -6,20 +6,19 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state ={
-      offices:[
-        { name:'office1' },
-        { name:'office2' }
-      ]
+      offices:[]
     };
   }
   onSearchTermChange(term) {
     console.log('search term change:', term);
+    const offices = term.split('').map( item => { return { name:'office_' + item} });
+    this.setState({ offices });
   }
   render() {
     return (
       <div>
         <h4>办公室查询</h4>
-        <SearchBar onSearchTermChange={ this.onSearchTermChange }/>
+        <SearchBar onSearchTermChange={ this.onSearchTermChange.bind(this) }/>
         <OfficeList offices={ this.state.offices }/>
       </div>
     );
