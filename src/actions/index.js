f04583892b1192e193f253553bbe4b4ccf4777ae
leapon <leapon@gmail.com>
2017-08-10 11:53:47 -0400

    add reducer for active office; add active office reducer to reducers/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 export function selectOffice(office) {
-  //console.log('office selected:', office.name);
+  console.log('office selected:', office.name);
   return {
     type: 'OFFICE_SELECTED',
     payload: office
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import OfficesReducer from './reducer_offices';
+import ActiveOfficeReducer from './reducer_active_office.js';
 
 const rootReducer = combineReducers({
-  // state: (state = {}) => state
-  offices: OfficesReducer
+  offices: OfficesReducer,
+  activeOffice: ActiveOfficeReducer
 });
 
 export default rootReducer;
