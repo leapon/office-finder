816662cbc4570769e4cef097b45c951dbbb63218
leapon <leapon@gmail.com>
2017-08-12 10:52:02 -0400

    use axios to search office in actions/index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,9 @@
+import axios from 'axios';
+
+const SEARCH_OFFICE = 'SEARCH_OFFICE';
 const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiYWRtaW5AbGVhcG9uLmNvbSIsImlhdCI6MTUwMjU0NzU0M30.xQ93S6BTYKTCuPFubnNb_Gk9jyiVV0S4iKcorIvmPKE';
+
+const OFFICE_SEARCH_URL = `localhost:8010/data/office/search?token=${API_KEY}`;
 
 export function selectOffice(office) {
   console.log('office selected:', office.name);
@@ -6,4 +11,14 @@ export function selectOffice(office) {
     type: 'OFFICE_SELECTED',
     payload: office
   };
+}
+
+export function searchOffice(term) {
+  const url = `${OFFICE_SEARCH_URL}?term=${term}`;
+  console.log('searchOffice url:', url);
+  const request = axios.get(url);
+  return {
+    type: SEARCH_OFFICE,
+    payload: request
+  }
 }
