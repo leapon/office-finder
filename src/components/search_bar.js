164d44358677be38b685cd70d2b312a3789449cd
leapon <leapon@gmail.com>
2017-08-12 10:54:10 -0400

    cleanup search_bar to set bound function in constructor

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,6 +5,8 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = { term:'' };
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onChange = this.onChange.bind(this)
   }
   onChange(event) {
     const term = event.target.value;
@@ -21,7 +23,7 @@ class SearchBar extends Component {
           <input
             className="form-control"
             value= { this.state.term }
-            onChange={ this.onChange.bind(this) } />
+            onChange={ this.onChange } />
           <span className="input-group-btn">
             <button type="submit" className="btn btn-secondary">Search</button>
           </span>
