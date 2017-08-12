cb6f67de47840816fdd084318e2725705af9f5fb
leapon <leapon@gmail.com>
2017-08-12 10:43:57 -0400

    add API_KEY to actions/index; redux-promise in package.json; ReduxPromise to middleware in src/index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiYWRtaW5AbGVhcG9uLmNvbSIsImlhdCI6MTUwMjU0NzU0M30.xQ93S6BTYKTCuPFubnNb_Gk9jyiVV0S4iKcorIvmPKE';
+
 export function selectOffice(office) {
   console.log('office selected:', office.name);
   return {
