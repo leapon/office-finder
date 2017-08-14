7bbaecc2d508a1ae02ab620b30424148d2d3c20a
leapon <leapon@gmail.com>
2017-08-14 14:10:00 -0400

    show office id in office_booking

--- src/components/office_booking.js
+++ src/components/office_booking.js
@@ -4,15 +4,20 @@ import { connect } from 'react-redux';
 
 class BookingForm extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = { id:'' };
   }
   componentDidMount() {
     const { id } = this.props.match.params;
+    this.setState({ id });
     console.log('>>> BookingForm component mounted - id:', id);
   }
   render() {
     return (
-      <div>Booking Form</div>
+      <div>
+        <div>Booking Form</div>
+        <div>Office id: { this.state.id }</div>
+      </div>
     );
   }
 }
