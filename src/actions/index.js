858e8ba94468474795456e6423df49f7d6d73be9
leapon <leapon@gmail.com>
2017-08-13 22:07:03 -0400

    fix search query url - 2nd parameter use & instead of ? again

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const SEARCH_OFFICE = 'SEARCH_OFFICE';
 const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiYWRtaW5AbGVhcG9uLmNvbSIsImlhdCI6MTUwMjY3MDYwMn0.yWXOFOaddAkuSKPMBaoONzyOz6zSjDKaNSRb1gk3Lfk';
 
-const OFFICE_SEARCH_URL = `localhost:9020/data/office/search?token=${API_KEY}`;
+const OFFICE_SEARCH_URL = `http://localhost:9020/data/office/search?token=${API_KEY}`;
 
 export function selectOffice(office) {
   console.log('office selected:', office.name);
@@ -14,7 +14,7 @@ export function selectOffice(office) {
 }
 
 export function searchOffice(term) {
-  const url = `${OFFICE_SEARCH_URL}?term=${term}`;
+  const url = `${OFFICE_SEARCH_URL}&term=${term}`;
   console.log('searchOffice url:', url);
   const request = axios.get(url);
   return {
