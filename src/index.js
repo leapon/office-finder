a5912673eb75921284cd326e28dec9686b65a283
leapon <leapon@gmail.com>
2017-08-14 07:45:27 -0400

    use Switch in react-router-dom to show corresponding component for given route

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import App from './components/app';
 import Hello from './components/hello';
@@ -16,8 +16,10 @@ ReactDOM.render(
     <BrowserRouter>
       <div>
         <h4>header</h4>
-        <Route path="/" component={App} />
-        <Route path="/hello" component={Hello} />
+        <Switch>
+          <Route path="/hello" component={Hello} />
+          <Route path="/" component={App} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>,
