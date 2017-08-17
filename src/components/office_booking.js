3a0482e69259ec711f79893af46414ff78278cd2
leapon <leapon@gmail.com>
2017-08-17 08:06:31 -0400

    add form and submit button in office_booking component

--- src/components/office_booking.js
+++ src/components/office_booking.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
 
 class BookingForm extends Component {
   constructor(props) {
@@ -12,11 +13,19 @@ class BookingForm extends Component {
     this.setState({ id });
     console.log('>>> BookingForm component mounted - id:', id);
   }
+  onSubmit(event) {
+    event.preventDefault();
+    console.log('>>> office booking form submit');
+  }
   render() {
     return (
       <div>
         <div>Booking Form</div>
         <div>Office id: { this.state.id }</div>
+        <hr/>
+        <form onSubmit={this.onSubmit}>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
       </div>
     );
   }
