65862172207be216adac8e365b002de0e72a7330
leapon <leapon@gmail.com>
2017-08-22 22:15:24 -0400

    add active booking reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { reducer as formReducer } from 'redux-form';
 import ActiveOfficeReducer from './reducer_active_office.js';
 import SearchOfficesReducer from './reducer_search_offices.js';
 import BookingReducer from './reducer_booking.js';
+import ActiveBookingReducer from './reducer_active_booking.js';
 
 const rootReducer = combineReducers({
+  form: formReducer,
   activeOffice: ActiveOfficeReducer,
   offices: SearchOfficesReducer,
-  form: formReducer
+  activeBooking: ActiveBookingReducer
 });
 
 export default rootReducer;
