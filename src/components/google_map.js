580b1fbf136176ef02cccf90a3f679fa43cf980f
leapon <leapon@gmail.com>
2017-08-22 22:39:06 -0400

    fix google map display update by adding componentDidUpdate

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -4,19 +4,29 @@ class GoogleMap extends Component {
   constructor(props) {
     super(props);
   }
-  componentDidMount() {
+  displayMap() {
     new google.maps.Map(this.refs.map, {
-      zoom: 12,
+      zoom: 18,
       center: {
         lat: this.props.lat,
         lng: this.props.lng
       }
     });
   }
+  componentDidMount() {
+    this.displayMap();
+  }
+  componentDidUpdate() {
+    this.displayMap();
+  }
   render() {
     return (
-      <div ref="map" className="office-google-map">
-      map
+      <div>
+        <div>
+          lat: { this.props.lat }, lng: { this.props.lng }
+        </div>
+        <div ref="map" className="office-google-map">
+        </div>
       </div>
     )
   };
