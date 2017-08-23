54df93af8bd1950785e24bfb2498fbfc96864753
leapon <leapon@gmail.com>
2017-08-22 21:39:41 -0400

    add getOfficeDetail and getBookingDetail actions in actions/index

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,10 +2,14 @@ import axios from 'axios';
 
 const SEARCH_OFFICE = 'SEARCH_OFFICE';
 const BOOK_OFFICE = 'BOOK_OFFICE';
+const OFFICE_DETAIL = 'OFFICE_DETAIL';
+const BOOKING_DETAIL = 'BOOKING_DETAIL';
 const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiYWRtaW5AbGVhcG9uLmNvbSIsImlhdCI6MTUwMjY3MDYwMn0.yWXOFOaddAkuSKPMBaoONzyOz6zSjDKaNSRb1gk3Lfk';
 
 const OFFICE_SEARCH_URL = `http://localhost:9020/data/office/search?token=${API_KEY}`;
+const OFFICE_DETAIL_URL = `http://localhost:9020/data/office/get?token=${API_KEY}`;
 const OFFICE_BOOK_URL = `http://localhost:9020/data/office/book?token=${API_KEY}`;
+const BOOKING_DETAIL_URL = `http://localhost:9020/data/booking/get?token=${API_KEY}`;
 
 export function selectOffice(office) {
   console.log('office selected:', office.name);
@@ -25,6 +29,16 @@ export function searchOffice(term) {
   }
 }
 
+export function getOfficeDetail(id) {
+  const url = `${OFFICE_DETAIL_URL}&id=${id}`;
+  console.log('getOfficeDetail url:', url);
+  const request = axios.get(url);
+  return {
+    type: OFFICE_DETAIL,
+    payload: request
+  }
+}
+
 export function bookOffice(values, callback) {
   const url = `${OFFICE_BOOK_URL}`;
   console.log('bookOffice url:', url);
@@ -33,6 +47,16 @@ export function bookOffice(values, callback) {
     .then((response) => callback(response.data));
   return {
     type: BOOK_OFFICE,
+    payload: request
+  }
+}
+
+export function getBookingDetail(id) {
+  const url = `${BOOKING_DETAIL_URL}&id=${id}`;
+  console.log('getBookingDetail url:', url);
+  const request = axios.get(url);
+  return {
+    type: BOOKING_DETAIL,
     payload: request
   }
 }
