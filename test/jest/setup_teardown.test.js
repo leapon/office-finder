55c41f7655dd384cd0bdbc52ee23ccadcb065d1f
leapon <leapon@gmail.com>
2017-08-24 11:16:54 -0400

    add setup and testdown test

---
+++ test/jest/setup_teardown.test.js
@@ -0,0 +1,32 @@
+function initializeCityDatabase () { return true; } 
+function isCity () { return true; } 
+function initializeFoodDatabase () { return true; } 
+function isValidCityFoodPair () { return true; } 
+
+// Applies to all tests in this file
+beforeEach(() => {
+  return initializeCityDatabase();
+});
+
+test('city database has Vienna', () => {
+  expect(isCity('Vienna')).toBeTruthy();
+});
+
+test('city database has San Juan', () => {
+  expect(isCity('San Juan')).toBeTruthy();
+});
+
+describe('matching cities to foods', () => {
+  // Applies only to tests in this describe block
+  beforeEach(() => {
+    return initializeFoodDatabase();
+  });
+
+  test('Vienna <3 sausage', () => {
+    expect(isValidCityFoodPair('Vienna', 'Wiener Schnitzel')).toBe(true);
+  });
+
+  test('San Juan <3 plantains', () => {
+    expect(isValidCityFoodPair('San Juan', 'Mofongo')).toBe(true);
+  });
+});
