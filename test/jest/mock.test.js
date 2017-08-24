6d800cafe3d717324312db84ed30d9251eaa304c
leapon <leapon@gmail.com>
2017-08-24 11:27:54 -0400

    mock return values

--- test/jest/mock.test.js
+++ test/jest/mock.test.js
@@ -38,3 +38,16 @@ test('mock property test', () => {
   // This function was instantiated exactly twice
   expect(myMock.mock.instances.length).toBe(2);
 })
+
+test('mock return value test', () => {
+  const myMock = jest.fn();
+  console.log(myMock());
+  // > undefined
+
+  myMock.mockReturnValueOnce(10)
+   .mockReturnValueOnce('x')
+   .mockReturnValue(true);
+
+  console.log(myMock(), myMock(), myMock(), myMock());
+  // > 10, 'x', true, true
+})
