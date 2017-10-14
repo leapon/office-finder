84205e42cb734c5e572b2a017efe5638ef243c97
Danieldylan1 <shadowbox826@gmail.com>
2017-10-14 19:10:39 -0400

    treeview

--- src/components/treeview.js
+++ src/components/treeview.js
@@ -44,6 +44,64 @@ Nodes:
     { display:'Son of C', level:3 }
 ]
 */
-class Treeview extends React.Component {
 
+// Take into account the order of the elements
+// Check if there is children
+// Create ids relating to level and use ABC letters to track
+// A for first node, AA for nested nodes, AAA, AAAA, etc.
+class TreeView extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      layout: props.layout
+    }
+  render() {
+    var levels = []
+    var nodeArr = []
+    var lowestLevel = 0
+    var numNodes = this.state.layout.length;
+    for (object in this.state.layout) {
+      var level = Object.keys(object)[1];
+      levels.push(level)
+    }
+    for (x in this.state.layout) {
+      if (x.level > lowestLevel) {
+          var lowestLevel = x
+      }
+    }
+
+    for(var i = 0; i < numNodes; i++) {
+      let hasChildren
+      let nodeId
+      if ((this.state.layout[i]).level < lowestLevel) {
+        hasChildren = true;
+      }
+      if (!((this.state.layout[i]).level in levels)) {
+        nodeId = "A".repeat(this.state.layout[i].level)
+      }
+      nodeArr.push(
+        <TreeNode
+          level = (this.state.layout[i]).level
+          display = (this.state.layout[i]).display
+          hasChildren = hasChildren
+          id = nodeId
+        />
+        )
+      }
+    }
+  }
+}
+class TreeNode extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      level: props.level,
+      display: props.display,
+      hasChildren: props.hasChildren,
+      children: props.children,
+      id: props.id
+    }
+  }
 }
